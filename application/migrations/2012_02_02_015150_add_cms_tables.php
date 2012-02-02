5acb5860e156fefd0d5ff54ebca3eb291de11811
Koen Schmeets <k.schmeets@gmail.com>
2012-02-02 15:19:56 +0100

    Adding migrations

---
+++ application/migrations/2012_02_02_015150_add_cms_tables.php
@@ -0,0 +1,91 @@
+<?php
+
+class Add_Cms_Tables {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('pages', function($table)
+		{
+			$table->create();
+			$table->increments('id');
+			$table->string('uri');
+			$table->boolean('online');
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+		});
+
+		Schema::table('page_lang', function($table)
+		{
+			$table->create();
+			$table->increments('id');
+			$table->integer('page_id');
+			$table->integer('language_id');
+			$table->boolean('active');
+			$table->string('meta_title');
+			$table->string('meta_keyword');
+			$table->string('meta_description');
+			$table->string('title');
+			$table->text('content');
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+		});
+
+		Schema::table('regions', function($table)
+		{
+			$table->create();
+			$table->increments('id');
+			$table->integer('page_id');
+			$table->integer('language_id');
+			$table->boolean('active');
+			$table->text('content');
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+		});
+
+		Schema::table('assets', function($table)
+		{
+			$table->create();
+			$table->increments('id');
+			$table->string('uri');
+			$table->string('name');
+			$table->string('content_type');
+			$table->text('content');
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('pages', function($table)
+		{
+			$table->drop();
+		});
+
+		Schema::table('page_lang', function($table)
+		{
+			$table->drop();
+		});
+
+		Schema::table('regions', function($table)
+		{
+			$table->drop();
+		});
+
+		Schema::table('assets', function($table)
+		{
+			$table->drop();
+		});
+	}
+
+}
---
+++ application/migrations/2012_02_02_044227_add_language_table.php
@@ -0,0 +1,34 @@
+<?php
+
+class Add_Language_Table {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('languages', function($table)
+		{
+			$table->create();
+			$table->increments('id');
+			$table->string('language_key');
+			$table->string('name');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('languages', function($table)
+		{
+			$table->drop();
+		});
+	}
+
+}
