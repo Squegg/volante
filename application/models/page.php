7803050cac875f9ba86f342bc74a56f2f90e2751
Koen Schmeets <k.schmeets@gmail.com>
2012-02-06 16:11:26 +0100

    Adding page model

--- application/models/page.php
+++ application/models/page.php
@@ -3,6 +3,9 @@ class Page extends Model {
 
 	public static $timestamps = true;
 
+	public $rules = array(
+	);
+
 	public function children()
 	{
 		return $this->has_many('Page', 'parent_id');
@@ -11,6 +14,36 @@ class Page extends Model {
 	public function regions()
 	{
 		return $this->has_many('Region');
+	}
+
+	public function validate_and_insert()
+	{
+		$validator = new Validator(Input::all(), $this->rules);
+
+		if ($validator->valid())
+		{
+			$this->name = Input::get('name');
+			$this->type = Input::get('type');
+			$this->content = Input::get('content');
+			$this->layoutgroup_id = DB::table('layoutgroups')->where_null('module_id')->first()->id;
+			$this->save();
+		}
+
+		return $validator->errors;
+	}
+
+	public function validate_and_update()
+	{
+		$validator = new Validator(Input::all(), $this->rules);
+		if ($validator->valid())
+		{
+			$this->name = Input::get('name');
+			$this->type = Input::get('type');
+			$this->content = Input::get('content');
+			$this->save();
+		}
+
+		return $validator->errors;
 	}
 
 }
