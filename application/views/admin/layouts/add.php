da0370bbb1ff6eacedabd9cc1d7f5feb19ba803d
Koen Schmeets <k.schmeets@gmail.com>
2012-02-11 03:43:15 +0100

    Update application/views/admin/layouts/add.php

--- application/views/admin/layouts/add.php
+++ application/views/admin/layouts/add.php
@@ -4,7 +4,7 @@
 	</div>
 	<?= Form::open('admin/layouts/add', 'POST', array('class' => 'form-horizontal')) ?>
 		<?= Form::field('text', 'name', __('admin_layouts.add.form.name'), array(Input::old('name')), array('error' => $errors->first('name'))) ?>
-		<?= Form::field('select', 'type', __('admin_layouts.add.form.type'), array(array_merge(array(''), Layout::$types)), array('error' => $errors->first('type'))) ?>
+		<?= Form::field('select', 'type', __('admin_layouts.add.form.type'), array(Layout::$types), array('error' => $errors->first('type'))) ?>
 		<?= Form::field('textarea', 'content', __('admin_layouts.add.form.content'), array(Input::old('content'), array('style' => 'width: 770px; height: 350px')), array('error' => $errors->first('content'))) ?>
 		<?= Form::actions(array(Form::submit(__('admin_layouts.add.form.submit'), array('class' => 'btn btn-large btn-primary')))) ?>
 	<?= Form::close() ?>
